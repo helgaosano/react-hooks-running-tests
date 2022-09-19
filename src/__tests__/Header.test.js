6fcb59a99c8e6cd414c74f9147f74dcf5284931d
tania <taniaosano15@gmail.com>
2022-09-19 03:31:27 +0300

    Done

--- src/__tests__/Header.test.js
+++ src/__tests__/Header.test.js
@@ -1,3 +1,4 @@
+{}jest
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import React from "react";
@@ -7,5 +8,5 @@ import Header from "../components/Header";
 test("displays the text 'hello from the Header!'", () => {
   render(<Header />);
 
-  expect(screen.queryByText("hello from the Header!")).toBeInTheDocument();
+  expect(screen.queryByText("hello from the Header!")).not.toBeInTheDocument();
 });
